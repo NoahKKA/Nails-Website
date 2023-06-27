8b147cdd4c7b874d6397f423cfe400c28e004f55
Gavin Sullivan <gavintsullivan@gmail.com>
2023-06-26 21:30:04 -0700

    Updated Link

--- src/pages/home-page/TopBar.jsx
+++ src/pages/home-page/TopBar.jsx
@@ -12,7 +12,7 @@ function TopBar() {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="https://youtu.be/dQw4w9WgXcQ">Link</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
