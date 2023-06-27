befd157a0c12801ae2725eec8a776e2262510741
NoahKKA <noahhhhh.a@gmail.com>
2023-06-26 20:37:31 -0700

    finished making server template

---
+++ server/server.js
@@ -0,0 +1,17 @@
+//DEPENDENCIES
+const express = require('express')
+const app = express()
+const { Sequelize } = require('sequelize')
+const path = require('path')
+const cors = require('cors')
+
+
+//MIDDLEWARE
+require('dotenv').config()
+app.use(cors())
+app.use()(express.urlencoded({ extended:false }));
+
+// LISTEN
+app.listen(4005, () => {
+    console.log("Server is running on port 4005")
+})
