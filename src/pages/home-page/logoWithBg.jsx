183461cca4d336f620f612c94ea6de2d7f78215a
Kyle <kylev006@gmail.com>
2023-06-27 13:45:10 -0700

    fixed navbar

--- src/pages/home-page/logoWithBg.jsx
+++ src/pages/home-page/logoWithBg.jsx
@@ -1,11 +1,11 @@
 //Imports
-import BgImg from "./nail.jpg";
+import BgImg from "../img/nail.jpg";
 //Function
 function MidImgLogo() {
     return (
         <body>
-            <div>//test
-                <img className="mx-auto" src={BgImg} alt="Logo" />;
+            <div>
+                <img className=" py-25 mx-auto min-w-lg" src={BgImg} alt="Logo" />;
             </div>
             
         </body>
