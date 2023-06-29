a4ec0c72047258ef007a38e51563d7fa1713821d
Kyle <kylev006@gmail.com>
2023-06-28 18:28:22 -0700

    added extra card

--- src/pages/home-page/InfoCards.jsx
+++ src/pages/home-page/InfoCards.jsx
@@ -8,16 +8,14 @@ import Acr from "../img/acrylicNail.jpg";
 //Function
 function InfoCards() {
     return (
-        <CardGroup className="max-w-1">
-            <Card className="max-w-1" >
+        <CardGroup className="max-w-1  py-5 ">
+            <Card className="max-w-1">
                 <img className="mx-auto max-w-1" src={Gel} alt="Logo" />
                 <Card.Body>
                     <Card.Title className="mx-auto">Gel Nails</Card.Title>
                 </Card.Body>
                 <ListGroup className="list-group-flush mx-auto">
-                    <ListGroup.Item>
-                      glossy and durable finish
-                    </ListGroup.Item>
+                    <ListGroup.Item>glossy and durable finish</ListGroup.Item>
                     <ListGroup.Item>
                         lasting longer than nail polish
                     </ListGroup.Item>
@@ -27,14 +25,31 @@ function InfoCards() {
                     <Card.Link href="#Calender">Schedule</Card.Link>
                 </Card.Body>
             </Card>
-            <Card className="max-w-1" >
+            <Card className="max-w-1">
                 <img className="mx-auto max-w-1" src={Acr} alt="Logo" />
                 <Card.Body>
-                    <Card.Title className="mx-auto">Gel Nails</Card.Title>
+                    <Card.Title className="mx-auto">Acrylic</Card.Title>
                 </Card.Body>
                 <ListGroup className="list-group-flush mx-auto">
                     <ListGroup.Item>durability and strength</ListGroup.Item>
                     <ListGroup.Item>customizable and versatile</ListGroup.Item>
+                    <ListGroup.Item>$1,000,000</ListGroup.Item>
+                </ListGroup>
+                <Card.Body>
+                    <Card.Link href="#Calender">Schedule</Card.Link>
+                </Card.Body>
+            </Card>
+
+            <Card className="max-w-1">
+                <img className="mx-auto max-w-1" src={Gel} alt="Logo" />
+                <Card.Body>
+                    <Card.Title className="mx-auto">Gel Nails</Card.Title>
+                </Card.Body>
+                <ListGroup className="list-group-flush mx-auto">
+                    <ListGroup.Item>glossy and durable finish</ListGroup.Item>
+                    <ListGroup.Item>
+                        lasting longer than nail polish
+                    </ListGroup.Item>
                     <ListGroup.Item>$1,000,000</ListGroup.Item>
                 </ListGroup>
                 <Card.Body>
