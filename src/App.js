178b1c78314ee65766fddb3e02e5e5952f943ae9
NoahKKA <noahhhhh.a@gmail.com>
2023-07-01 15:16:27 -0700

    working on emailform

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import HomePage from "./pages/HomePage";
 import CalendarPage from "./pages/CalanderPage";
 import Error404Page from "./pages/Error404";
+import SchedulePage from "./pages/SchedulePage";
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
                 <Routes>
                     <Route path='/' element={<HomePage/>}/>
                     <Route path='/Calendar' element={<CalendarPage/>}/>
+                    <Route path='/Schedule' element={<SchedulePage/>}/>
                     <Route path='*' element={<Error404Page/>}/>
                 </Routes>           
             </Router>
