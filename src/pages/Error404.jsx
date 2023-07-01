a9cde4aa460a334984bcb9548ee36ea2798d7658
Ryanhu-hub <ryanhuot20@gmail.com>
2023-07-01 11:52:26 -0700

    added error404 within pages folder

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-
 function Error404() {
   return (
-    <div>Error404</div>
-  )
+    <Def>
+      <main>
+        <h1>404: PAGE NOT FOUND</h1>
+        <p>Oops, sorry, we can't find this page!</p>
+        <a href="/">Click Here to go Home</a>
+        <div>
+          <img src="/images/sad_cat.jpg" alt="Sad Cat" />
+        </div>
+      </main>
+    </Def>
+  );
 }
 
-export default Error404
+
+export default Error404;
