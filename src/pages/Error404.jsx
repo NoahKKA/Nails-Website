055686cc081e7768e4f5d1b8a260c49186fe297f
Gavin Sullivan <gavintsullivan@gmail.com>
2023-07-01 12:17:15 -0700

    Updated 404 message and img

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 function Error404() {
   return (
-    <div>Error404</div>
+    <>
+    <div>
+        Sorry, the page you are trying to reach is unavailable. <br></br>
+        <img src="Nails-Website\src\pages\img\nail-404-image.jpg" alt="NAIL 404" />
+    </div>
+
+    </>
   )
 }
 
