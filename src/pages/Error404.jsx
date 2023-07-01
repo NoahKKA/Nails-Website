d221ea62538cf0182fe27de99856286b3b239f7a
Ryanhu-hub <ryanhuot20@gmail.com>
2023-07-01 15:18:33 -0700

    added import for css

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,9 +1,10 @@
-import Nail404 from "../assets/images/nail-404-image.jpg" 
+import Nail404 from "../assets/images/nail-404-image.jpg"
+import "../App.css"
 
 function Error404Page() {
   return (
     <>
-    <div>
+    <div className="errorPage">
         Sorry, the page you are trying to reach is unavailable. <br></br>
         <img src={Nail404} alt="NAIL 404" />
     </div>
