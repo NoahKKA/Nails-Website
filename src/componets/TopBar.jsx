49f8a5da2b0d6a9cd4c6f6c4f4ef18b40df5f801
NoahKKA <noahhhhh.a@gmail.com>
2023-07-05 18:16:29 -0700

    edited navbar to look more pleasing also added dropdown for darkmode

--- src/componets/TopBar.jsx
+++ src/componets/TopBar.jsx
@@ -1,35 +1,84 @@
 //Imports
 import Nav from "react-bootstrap/Nav";
-import { Calendar, Cart4, Moon, House} from "react-bootstrap-icons";
+import {
+  Calendar,
+  Cart4,
+  MoonStars,
+  MoonStarsFill,
+  House,
+  QuestionCircle
+} from "react-bootstrap-icons";
+import { Dropdown } from "react-bootstrap";
 
 //Function
 function TopBar() {
-    return (
-        <div>
-            <Nav className="py-100 justify-content-center bg-slate-400">
-                <h2>
-                <Nav.Link  href="/" className="text-black px-4">
-                    <House />
-                </Nav.Link>
-                </h2>
-                <h2>
-                <Nav.Link href="/Shopping" className="text-black px-4">
-                    <Cart4 />
-                </Nav.Link>
-                </h2>
-                <h2>
-                <Nav.Link href="/Calendar" className="text-black px-4">
-                    <Calendar />
-                </Nav.Link>
-                </h2>
-                <h2>
-                <Nav.Link href="/DarkMode" className="text-black px-4">
-                    <Moon />
-                </Nav.Link>
-                </h2>
-            </Nav>
+  return (
+    <div>
+      <Nav className="py-100 justify-content-between bg-slate-400">
+        <h2 style={{ marginLeft: "1rem" }}>
+          <Nav.Link
+            href="/"
+            className="text-black px-4"
+            data-toggle="tooltip"
+            data-placement="bottom"
+            title="Home"
+          >
+            <House />
+          </Nav.Link>
+        </h2>
+        <div className="d-flex align-items-center">
+          <h2 className="text-center" style={{ marginRight: "1rem" }}>
+            <Nav.Link
+              href="/Shopping"
+              className="text-black px-4"
+              data-toggle="tooltip"
+              data-placement="bottom"
+              title="Shopping"
+            >
+              <Cart4 />
+            </Nav.Link>
+          </h2>
+          <h2 className="text-center" style={{ marginRight: "1rem" }}>
+            <Nav.Link
+              href="/Calendar"
+              className="text-black px-4"
+              data-toggle="tooltip"
+              data-placement="bottom"
+              title="Calendar"
+            >
+              <Calendar />
+            </Nav.Link>
+          </h2>
+          <h2 className="text-center" style={{ marginRight: "1rem" }}>
+            <Dropdown>
+              <Dropdown.Toggle
+                className="custom-toggle text-black px-4"
+                data-toggle="tooltip"
+                data-placement="bottom"
+                title="DarkMode"
+              >
+                <MoonStars />
+              </Dropdown.Toggle>
+              <Dropdown.Menu className="dropdown-menu-right">
+                <Dropdown.Item>
+                  <MoonStars />
+                  <span className="mt-2">Light Mode</span>
+                </Dropdown.Item>
+                <Dropdown.Item>
+                  <MoonStarsFill />
+                  <span className="mt-2">Dark Mode</span>
+                </Dropdown.Item>
+                <Dropdown.Item>
+                  <QuestionCircle />
+                  <span className="mt-2">Auto</span>
+                </Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </h2>
         </div>
-    );
+      </Nav>
+    </div>
+  );
 }
 
 //Export
