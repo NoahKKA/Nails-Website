11e03b837ebaa4dae464f5e3fae5ac8ad18198f1
Kyle <kylev006@gmail.com>
2023-07-06 15:50:55 -0700

    added buttons to add an appointment

--- src/componets/Products.jsx
+++ src/componets/Products.jsx
@@ -6,6 +6,7 @@ import cPro3 from "../assets/images/IMG_5.png";
 function Products() {
     return (
         <Carousel className=" py-10">
+            <h2 className="text-center"> Other work we have done</h2>
             <Carousel.Item interval={2000} >
                 <img 
                 className="d-block max-h-[100%] max-w-[100%]" 
