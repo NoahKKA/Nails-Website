c882725d3d62dfa18e28287dab7546702ddf2d59
Kyle <kylev006@gmail.com>
2023-07-05 22:05:17 -0700

    updated the cards to more better look

--- src/componets/TopBar.jsx
+++ src/componets/TopBar.jsx
@@ -15,7 +15,7 @@ import { Dropdown } from "react-bootstrap";
 function TopBar() {
   return (
     <div>
-      <Navbar className="fixed-top py-100 justify-content-between bg-gradient-to-r from-green-300 via-cyan-300 to-green-300">
+      <Navbar className="fixed-top max-  justify-content-between bg-gradient-to-r from-green-300 via-cyan-300 to-green-300">
         <h2 style={{ marginLeft: "1rem" }}>
           <Nav.Link
             href="/"
