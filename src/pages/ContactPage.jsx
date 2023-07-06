6c10b8da70f8d8fe3449c6dbcee1ed5690a88a6c
Ryanhu-hub <ryanhuot20@gmail.com>
2023-07-05 20:06:55 -0700

    adjusted 404 page and added consts and functions to contact page

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -4,6 +4,55 @@ import TopBar from "../componets/TopBar.jsx";
 import Footer from "../componets/Footer.jsx"
 import Default from "./DefaultPage";
 
-const ContactPage = {}
+const ContactPage = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
 
-export default Default
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add your form submission logic here
+    console.log('Form submitted:', name, email, message);
+    // Reset form fields
+    setName('');
+    setEmail('');
+    setMessage('');
+  };
+
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="message">Message</label>
+          <textarea
+            id="message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          ></textarea>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default ContactPage;
