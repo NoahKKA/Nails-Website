477397ae7d34bcd5c5fa943c581049bef8ce670c
NoahKKA <noahhhhh.a@gmail.com>
2023-07-10 13:07:52 -0700

    Edit Appointment works except the quantity and accessory change

--- src/pages/EditPage.jsx
+++ src/pages/EditPage.jsx
@@ -3,10 +3,30 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { supabase } from "../SupaBaseClient";
 
+//Makes timeblocks starting from 8am to 6pm in HH:MM
+const timeBlocks = [];
+const startTime = new Date("1970/01/01 08:00").getTime();
+const endTime = new Date("1970/01/01 18:00").getTime();
+const hourMilliseconds = 60 * 30 * 1000;
+
+for (
+  let timestamp = startTime;
+  timestamp <= endTime;
+  timestamp += hourMilliseconds
+) {
+  const time = new Date(timestamp).toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  timeBlocks.push({ label: time, value: time });
+}
+
 function EditPage() {
   const { id } = useParams();
   const [appointment, setAppointment] = useState({});
-  const [accessories, setAccessories] = useState([]);
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [selectedStartTime, setSelectedStartTime] = useState("");
+  const [dropdownVisible, setDropdownVisible] = useState(false);
 
   const fetchAppointmentById = async () => {
     try {
@@ -20,25 +40,14 @@ function EditPage() {
       } else {
         console.log("Fetched data:", data);
         setAppointment(data);
-      }
-  
-      if (data && data.selectedAccessories && data.selectedAccessories.length > 0) {
-        const { data: accessoryData, error: accessoryError } = await supabase
-          .from("accesories")
-          .select("*")
-          .in("accesories_id", data.selectedAccessories);
-        if (accessoryError) {
-          console.log("Error fetching accessory data:", accessoryError.message);
-        } else {
-          console.log("Fetched accessory data:", accessoryData);
-          setAccessories(accessoryData);
-        }
+        setSelectedDate(new Date(data.date));
+        setSelectedStartTime(data.startTime);
       }
     } catch (err) {
       console.log("Error occurred during data fetching:", err);
     }
   };
-  
+
   useEffect(() => {
     fetchAppointmentById();
   }, [id]);
@@ -59,32 +68,153 @@ function EditPage() {
     }
   };
 
+  const handleSaveChanges = async () => {
+    try {
+      const { error } = await supabase
+        .from("appointments")
+        .update({
+          name: appointment.name,
+          date: selectedDate.toISOString().split("T")[0],
+          startTime: selectedStartTime,
+          endTime: endTime,
+        })
+        .eq("appointmentId", id);
+      if (error) {
+        throw error;
+      }
+
+      console.log("Appointment updated successfully");
+      window.location.href = "/Calendar";
+    } catch (err) {
+      console.log("Error updating appointment:", err);
+    }
+  };
+
+  const handleStartTimeChange = (e) => {
+    const startTime = e.target.value;
+    setSelectedStartTime(startTime);
+
+    // Calculate end time based on start time
+    const startTimestamp = new Date(`1970/01/01 ${startTime}`).getTime();
+    const endTimestamp = startTimestamp + 4 * 60 * 60 * 1000; // 4 hours
+    const endTime = new Date(endTimestamp).toLocaleTimeString([], {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+
+    // Update appointment data
+    setAppointment((prevFormData) => ({
+      ...prevFormData,
+      startTime,
+      endTime,
+    }));
+
+    setDropdownVisible(false);
+  };
+
   return (
     <Default fixed="bottom">
       {appointment.name && (
-        <div className="text-center mt-20">
-          <h4>{appointment.name}</h4>
-          <h2>{appointment.date}</h2>
-          <p>
-            <strong>Start Time:</strong> {appointment.startTime}
-          </p>
-          <p>
-            <strong>End Time:</strong> {appointment.endTime}
-          </p>
-          <p>
-            <strong>Price Estimate:</strong> ${appointment.priceEstimate}
-          </p>
+        <div className="d-flex justify-content-center align-items-center mt-20">
+          <div>
+            <h2
+              style={{
+                fontFamily: "Cormorant Upright serif",
+                fontWeight: "bolder",
+              }}
+            >
+              Schedule an Appointment
+            </h2>
+            <h4>Base fee of $60</h4>
+            <form className="container mt-5">
+              <div className="mb-3">
+                <label htmlFor="name" className="form-label">
+                  Edit Name:
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  className="form-control"
+                  placeholder="Enter Name"
+                  value={appointment.name}
+                  onChange={(e) =>
+                    setAppointment((prevAppointment) => ({
+                      ...prevAppointment,
+                      name: e.target.value,
+                    }))
+                  }
+                  required
+                />
+              </div>
 
-          {accessories.length > 0 && (
-            <div>
-              <h4>Selected Accessories:</h4>
-              <ul>
-                {accessories.map((accessory) => (
-                  <li key={accessory.accessoryId}>{accessory.name}</li>
-                ))}
-              </ul>
-            </div>
-          )}
+              <div className="mb-3">
+                <label htmlFor="date" className="form-label">
+                  Edit Date:
+                </label>
+                <div className="input-group">
+                  <input
+                    type="date"
+                    id="date"
+                    name="date"
+                    className="form-control"
+                    value={
+                      selectedDate
+                        ? selectedDate.toISOString().split("T")[0]
+                        : ""
+                    }
+                    onChange={(e) => setSelectedDate(new Date(e.target.value))}
+                    required
+                  />
+                </div>
+              </div>
+
+              <div className="mb-3">
+                <label htmlFor="start-time" className="form-label">
+                  Edit Start Time:
+                </label>
+                <div
+                  className="form-control"
+                  onClick={() => setDropdownVisible(!dropdownVisible)}
+                >
+                  {selectedStartTime || "Select Start Time"}
+                </div>
+                {dropdownVisible && (
+                  <select
+                    id="start-time"
+                    name="startTime"
+                    className="form-control"
+                    value={selectedStartTime}
+                    onChange={handleStartTimeChange}
+                    required
+                    size={7}
+                  >
+                    {timeBlocks.map((block) => (
+                      <option key={block.value} value={block.value}>
+                        {block.label}
+                      </option>
+                    ))}
+                  </select>
+                )}
+              </div>
+
+              <div className="mb-3">
+                <label htmlFor="end-time" className="form-label">
+                  End Time:
+                </label>
+                <input
+                  type="text"
+                  id="end-time"
+                  name="endTime"
+                  className="form-control"
+                  placeholder="End Time"
+                  value={appointment.endTime}
+                  readOnly
+                  required
+                />
+              </div>
+            </form>
+          </div>
         </div>
       )}
 
@@ -94,6 +224,9 @@ function EditPage() {
           className="btn btn-danger mb-5"
         >
           Delete Appointment
+        </button>
+        <button onClick={handleSaveChanges} className="btn btn-primary mb-5">
+          Save Changes
         </button>
       </div>
     </Default>
